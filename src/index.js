f046437607bde566f96ff681b609dcfd886ef0d4
Jitendra Kumar <jitendra.kumar_cs20@gla.ac.in>
2024-05-09 02:02:00 +0530

    Final

--- src/index.js
+++ src/index.js
@@ -117,12 +117,7 @@ const url = process.env.MONGO_URL;
 connectDB("mongodb+srv://jeet:123jeet321@jio.ydzozc6.mongodb.net/?retryWrites=true&w=majority&appName=jio");
 
 // Use cors middleware to allow all requests
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE , OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+app.use(cors());
 
 
 app.get("/", async (req, res) => {
